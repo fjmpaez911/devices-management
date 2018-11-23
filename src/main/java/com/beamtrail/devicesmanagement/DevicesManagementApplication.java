ffaa5510d7dda56cb2dae4ddbfdc330c8efe0111
Fran Martinez <francisco.martinez@starzplayarabia.com>
2018-11-23 15:39:25 +0400

    init

---
+++ src/main/java/com/beamtrail/devicesmanagement/DevicesManagementApplication.java
@@ -0,0 +1,12 @@
+package com.beamtrail.devicesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicesManagementApplication.class, args);
+	}
+}
